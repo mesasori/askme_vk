05cf65fcb2f9f22aa72ebb94ee085a5bb23eaa69
mesasori <screepya@gmail.com>
2023-11-14 06:15:43 +0300

    dz2 without pagination

--- app/views.py
+++ app/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+QUESTIONS = [
+    {
+        'id': i,
+        'title': f'Question {i}',
+        'content': f'Long lorem ipsum {i}',
+        'like': i + 1,
+        'tag': []
+    } for i in range(50)
+]
+
+for i in range(50):
+    if i % 2 == 0:
+        QUESTIONS[i]['tag'] = ['CSS', 'HTML']
+    else:
+        QUESTIONS[i]['tag'] = ['Python', 'Django']
+    if i % 3 == 0:
+        QUESTIONS[i]['tag'] = ['JavaScript', 'Voloshin', 'bender']
+
+ANSWERS = [
+    {
+        'id': i,
+        'content': f'Long lorem ipsum long lorem ipsum long lorem ipsum {i}',
+        'like': i + 1
+    } for i in range(3)
+]
+
+
+def paginate(objects, request, per_page=10):
+    paginator = Paginator(objects, per_page)
+    page = request.GET.get('page', 1)
+    return paginator.page(page)
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', {'questions': paginate(QUESTIONS, request)})
+
+
+def question(request, question_id):
+    item = QUESTIONS[question_id]
+    return render(request, 'question.html', {'question': item, 'answers': ANSWERS})
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
+
+
+def ask(request):
+    return render(request, 'ask.html')
+
+
+def hot(request):
+    return render(request, 'hot.html', {'questions': paginate(QUESTIONS, request)})
+
+
+def tag(request, selected_tag):
+    questions_list = []
+    for item in QUESTIONS:
+        if selected_tag in item['tag']:
+            questions_list.append(item)
+    return render(request, 'tag.html', {'questions': paginate(questions_list, request), 'selected_tag': selected_tag})
--- askme_vk/urls.py
+++ askme_vk/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('question/<int:question_id>', views.question, name='question'),
     path('admin/', admin.site.urls),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('ask/', views.ask, name="ask"),
+    path('hot/', views.hot, name='hot'),
+    path('tag/<selected_tag>', views.tag, name='tag')
 ]
