2396df999ba552f9beebcc979780fafd30ae6a5a
mesasori <screepya@gmail.com>
2023-11-15 17:46:37 +0300

    dz3 ready point 1

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Question, Tag, Answer, Profile, Like
 
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Tag)
+admin.site.register(Answer)
+admin.site.register(Profile)
+admin.site.register(Like)
