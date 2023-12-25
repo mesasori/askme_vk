59998faead304da07b7614519f172415a56210bf
mesasori <screepya@gmail.com>
2023-12-25 12:07:49 +0300

    full dz2. dz3 step 1

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from app import views
 from django.contrib import admin
+from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls)
+    path('', views.index, name='index'),
+    path('question/<int:question_id>', views.question, name='question'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('ask/', views.ask, name="ask"),
+    path('hot/', views.hot, name='hot'),
+    path('tag/<selected_tag>', views.tag, name='tag'),
+    path('settings/', views.settings, name='settings')
 ]
--- askme_vk/urls.py
+++ askme_vk/urls.py
@@ -1,14 +1,8 @@
-from django.urls import path
-from app import views
+from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('question/<int:question_id>', views.question, name='question'),
-    path('login/', views.login, name='login'),
-    path('signup/', views.signup, name='signup'),
-    path('ask/', views.ask, name="ask"),
-    path('hot/', views.hot, name='hot'),
-    path('tag/<selected_tag>', views.tag, name='tag'),
-    path('settings/', views.settings, name='settings')
+    path('', include('app.urls')),
+    path('admin/', admin.site.urls)
 ]
 
